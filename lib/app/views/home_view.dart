57605c72d17abe4d135c2ddeed6e98b01bb6ed8b
Eduardo Freitas <eduardoflorencio96@gmail.com>
2023-07-03 23:15:47 -0300

    feat: desafio

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -28,50 +28,52 @@ class _HomeViewState extends State<HomeView> {
         child: Padding(
           padding:
               const EdgeInsets.only(left: 30, right: 30, top: 100, bottom: 20),
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/logo.png',
-                width: 150,
-                height: 150,
-              ),
-              SizedBox(height: 50),
-              CurrencyBox(
-                selectedItem: homeController.toCurrency,
-                controller: toText,
-                items: homeController.currencies,
-                onChanged: (model) {
-                  setState(() {
-                    homeController.toCurrency = model!;
-                  });
-                },
-              ),
-              SizedBox(height: 10),
-              CurrencyBox(
-                selectedItem: homeController.fromCurrency,
-                controller: fromText,
-                items: homeController.currencies,
-                onChanged: (model) {
-                  setState(() {
-                    homeController.fromCurrency = model!;
-                  });
-                },
-              ),
-              SizedBox(height: 50),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: Colors.amber,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32),
-                  ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  width: 150,
+                  height: 150,
                 ),
-                onPressed: () {
-                  homeController.convert();
-                },
-                child: Text('CONVERTER'),
-              ),
-            ],
+                SizedBox(height: 50),
+                CurrencyBox(
+                  selectedItem: homeController.toCurrency,
+                  controller: toText,
+                  items: homeController.currencies,
+                  onChanged: (model) {
+                    setState(() {
+                      homeController.toCurrency = model!;
+                    });
+                  },
+                ),
+                SizedBox(height: 10),
+                CurrencyBox(
+                  selectedItem: homeController.fromCurrency,
+                  controller: fromText,
+                  items: homeController.currencies,
+                  onChanged: (model) {
+                    setState(() {
+                      homeController.fromCurrency = model!;
+                    });
+                  },
+                ),
+                SizedBox(height: 50),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.amber,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                  ),
+                  onPressed: () {
+                    homeController.convert();
+                  },
+                  child: Text('CONVERTER'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
